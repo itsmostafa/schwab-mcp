31e0db3052fc10acf2e8add758808b428d064fce
mostafa-abdo <abdelbaky.mostafa@gmail.com>
2019-05-31 16:24:02 -0700

    add perm mising elem to codility

--- codility/perm_missing_elem.py
+++ codility/perm_missing_elem.py
@@ -23,3 +23,23 @@ N is an integer within the range [0..100,000];
 the elements of A are all distinct;
 each element of array A is an integer within the range [1..(N + 1)]
 '''
+
+def solution(A):
+    length = len(A) + 1
+    return int(length*(1+length)/2 - sum(A))
+
+# Tests
+list_1 = [1, 2, 2, 3, 5, 6, 6]
+print(solution(list_1))
+
+list_2 = [7, 9, 8, 5, 7, 4]
+print(solution(list_2))
+
+list_3 = [2]
+print(solution(list_3))
+
+list_4 = []
+print(solution(list_4))
+
+list_5 = [2, 3, 1, 5]
+print(solution(list_5))
